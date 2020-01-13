fe35814196ceb65e8402a36c1a6912bd2cd8cfb9
Abhi Jain <devabhixda@gmail.com>
2020-01-13 17:05:39 +0530

    Add support for 4 servos
    Signed-off-by: Abhi Jain <devabhixda@gmail.com>

--- servo.ino
+++ servo.ino
@@ -4,22 +4,34 @@
 
 Servo myservo1;
 Servo myservo2;
+Servo myservo3;
+Servo myservo4;
 static const int servoPin1 = 13;
 static const int servoPin2 = 14;
+static const int servoPin3 = 12;
+static const int servoPin4 = 15;
 WiFiServer server(80);
 String header;
 String valueString1 = String(5);
 String valueString2 = String(5);
+String valueString3 = String(5);
+String valueString4 = String(5);
 int pos1 = 0;
 int pos2 = 0;
 int pos3 = 0;
 int pos4 = 0;
+int pos5 = 0;
+int pos6 = 0;
+int pos7 = 0;
+int pos8 = 0;
 void setup() {
   Serial.begin(115200);
   WiFiManager wifiManager;
   wifiManager.autoConnect("SRC");
   myservo1.attach(servoPin1);
   myservo2.attach(servoPin2);
+  myservo3.attach(servoPin3);
+  myservo4.attach(servoPin4);
   Serial.print("Connecting to ");
   // Print local IP address and start web server
   Serial.println("");
@@ -52,7 +64,7 @@ void loop(){
             client.println(".slider { width: 300px; }</style>");
             client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
             client.println("</head><body><h1>ESP32 with Servo</h1>");
-            
+
             client.println("<p>Position: <span id=\"servoPos1\"></span></p>");          
             client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider1\" id=\"servoSlider1\" onchange=\"servo1(this.value)\" value=\""+valueString1+"\"/>");         
             client.println("<script>var slider1 = document.getElementById(\"servoSlider1\");");
@@ -68,7 +80,22 @@ void loop(){
             client.println("slider2.oninput = function() { slider2.value = this.value; servoP2.innerHTML = this.value; }");
             client.println("$.ajaxSetup({timeout:1000}); function servo2(pos) { ");
             client.println("$.get(\"/?value2=\" + pos + \"&\"); {Connection: close};}</script>");
-            
+
+            client.println("<p>Position: <span id=\"servoPos3\"></span></p>");          
+            client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider3\" id=\"servoSlider3\" onchange=\"servo3(this.value)\" value=\""+valueString3+"\"/>");         
+            client.println("<script>var slider3 = document.getElementById(\"servoSlider3\");");
+            client.println("var servoP3 = document.getElementById(\"servoPos3\"); servoP3.innerHTML = slider3.value;");
+            client.println("slider3.oninput = function() { slider3.value = this.value; servoP3.innerHTML = this.value; }");
+            client.println("$.ajaxSetup({timeout:1000}); function servo3(pos) { ");
+            client.println("$.get(\"/?value3=\" + pos + \"&\"); {Connection: close};}</script>");
+
+            client.println("<p>Position: <span id=\"servoPos4\"></span></p>");          
+            client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider4\" id=\"servoSlider4\" onchange=\"servo4(this.value)\" value=\""+valueString4+"\"/>");         
+            client.println("<script>var slider4 = document.getElementById(\"servoSlider4\");");
+            client.println("var servoP4 = document.getElementById(\"servoPos4\"); servoP4.innerHTML = slider4.value;");
+            client.println("slider4.oninput = function() { slider4.value = this.value; servoP4.innerHTML = this.value; }");
+            client.println("$.ajaxSetup({timeout:1000}); function servo4(pos) { ");
+            client.println("$.get(\"/?value4=\" + pos + \"&\"); {Connection: close};}</script>");
             client.println("</body></html>");     
 
             //GET /?value=180& HTTP/1.1
@@ -91,6 +118,26 @@ void loop(){
               myservo2.write(valueString2.toInt());
               Serial.println("Second "+valueString2); 
             }
+
+            if(header.indexOf("GET /?value3=")>=0) {
+              pos5 = header.indexOf('=');
+              pos6 = header.indexOf('&');
+              valueString3 = header.substring(pos5+1, pos6);
+              
+              //Rotate the servo
+              myservo3.write(valueString3.toInt());
+              Serial.println("Second "+valueString3); 
+            }
+
+            if(header.indexOf("GET /?value4=")>=0) {
+              pos7 = header.indexOf('=');
+              pos8 = header.indexOf('&');
+              valueString4 = header.substring(pos7+1, pos8);
+              
+              //Rotate the servo
+              myservo4.write(valueString4.toInt());
+              Serial.println("Second "+valueString4); 
+            }                        
             // The HTTP response ends with another blank line
             client.println();
             break;
